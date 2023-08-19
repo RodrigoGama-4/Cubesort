c156fb208a9ae5ee2372a46bde11af6749aefe1a
rodrigogama <rodrigogamah4@gmail.com>
2023-08-18 21:12:12 -0300

    .

--- Cubesort.java
+++ Cubesort.java
@@ -1,107 +1,108 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
 
-public class Cubesort{
-
-    static void sortArray(int arr[], int n) {
-        Integer[] ar = new Integer[n];
-
-        for (int i = 0; i < n; i++) {
-            ar[i] = arr[i];
+public class CubesortRan {
+    public static void cubesort(int[] A, int p) {
+        
+        int n = A.length;
+        int subarraySize = n / p;
+        
+        // Step 1: Partition the data
+        int[][] subarrays = new int[p][];
+        for (int i = 0; i < p; i++) {
+            int[] subarray = Arrays.copyOfRange(A, i * subarraySize, (i + 1) * subarraySize);
+            subarrays[i] = subarray;
         }
-
-        Arrays.sort(ar, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                int x = (int) Math.pow(a, 3);
-                int y = (int) Math.pow(b, 3);
-                return Integer.compare(x, y);
+        
+        // Step 2: Sort each subarray in parallel
+        for (int[] subarray : subarrays) {
+            Arrays.sort(subarray);
+        }
+        
+        
+        for (int dim = 0; dim < Integer.numberOfTrailingZeros(p); dim++) {
+            for (int i = 0; i < p; i++) {
+                int partner = i ^ (1 << dim);
+                if (i < partner) {
+                    int[] merged = new int[subarraySize * 2];
+                    System.arraycopy(subarrays[i], 0, merged, 0, subarraySize);
+                    System.arraycopy(subarrays[partner], 0, merged, subarraySize, subarraySize);
+                    Arrays.sort(merged);
+                    System.arraycopy(merged, 0, subarrays[i], 0, subarraySize);
+                    System.arraycopy(merged, subarraySize, subarrays[partner], 0, subarraySize);
+                }
             }
-        });
-
-        for (int i = 0; i < n; i++) {
-            System.out.println(ar[i] + " ");
+        }
+        
+        
+        // Step 4: Copy the sorted subarrays back to the original array
+        int index = 0;
+        for (int[] subarray : subarrays) {
+            System.arraycopy(subarray, 0, A, index, subarraySize);
+            index += subarraySize;
         }
     }
 
-     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int[] inputSizes = {1000000};
+    public static void main(String[] args) {
+        int[] randomArray = new int[0];
+        int numeroComparacoes = 10000000;
+       
         
-        for (int n : inputSizes) {
-            System.out.println("Input Size: " + n);
-            
-            // Generate random input array
-            int[] randomArray = new int[n];
-            Random random = new Random();
-            for (int i = 0; i < n; i++) {
-                randomArray[i] = random.nextInt();
-            }
-            
-            // Generate decreasing input array
-            int[] decreasingArray = new int[n];
-            for (int i = 0; i < n; i++) {
-                decreasingArray[i] = n - i;
-            }
-
-            // Best case scenario: array is already sorted
-            int[] bestCaseArray = Arrays.copyOf(randomArray, n);
-            Arrays.sort(bestCaseArray);
-
-            // Worst case scenario: array is sorted in reverse order
-            int[] worstCaseArray = Arrays.copyOf(randomArray, n);
-            Arrays.sort(worstCaseArray);
-            for (int i = 0; i < n / 2; i++) {
-                int temp = worstCaseArray[i];
-                worstCaseArray[i] = worstCaseArray[n - i - 1];
-                worstCaseArray[n - i - 1] = temp;
-            }
-
-            // Measure time for sorting in different scenarios
-            long startTimeRandom, startTimeDecres, startTimeBest,
-            startTimeWorst,endTimeWorst, endTimeRandom, endTimeDecres, endTimeBest,
-            elapsedTimeDecres,elapsedTimeBest, elapsedTimeWorst,
-            elapsedTimeRandom;
-
-            System.out.println("Random Input:");
-            startTimeRandom = System.nanoTime();
-            sortArray(randomArray, n);
-            endTimeRandom = System.nanoTime();
-            elapsedTimeRandom = endTimeRandom - startTimeRandom;
-           
-
-            System.out.println("Decreasing Input:");
-            startTimeDecres = System.nanoTime();
-            sortArray(decreasingArray, n);
-            endTimeDecres = System.nanoTime();
-            elapsedTimeDecres = endTimeDecres - startTimeDecres;
-            
-
-            System.out.println("Best Case Input:");
-            startTimeBest = System.nanoTime();
-            sortArray(bestCaseArray, n);
-            endTimeBest = System.nanoTime();
-            elapsedTimeBest = endTimeBest - startTimeBest;
-            
-
-            System.out.println("Worst Case Input:");
-            startTimeWorst = System.nanoTime();
-            sortArray(worstCaseArray, n);
-            endTimeWorst = System.nanoTime();
-            elapsedTimeWorst = endTimeWorst - startTimeWorst;
-
-            // Converter tempos para segundos
-            double elapsedTimeRandomSeconds = (double) elapsedTimeRandom / 1_000_000_000.0;
-            double elapsedTimeDecresSeconds = (double) elapsedTimeDecres / 1_000_000_000.0;
-            double elapsedTimeBestSeconds = (double) elapsedTimeBest / 1_000_000_000.0;
-            double elapsedTimeWorstSeconds = (double) elapsedTimeWorst / 1_000_000_000.0;
-          
-            //tempos
-            System.out.println("Input aleatorio teve um tempo de " + elapsedTimeRandomSeconds + " s");
-            System.out.println("Input decrescente teve um tempo de " + elapsedTimeDecresSeconds + " s");
-            System.out.println("INput com melhor caso teve um tempo de " + elapsedTimeBestSeconds + " s");
-            System.out.println("Pior caso teve um tempo de " + elapsedTimeWorstSeconds + " s");
-
-            System.out.println();
+        System.out.println("Input Size: " + numeroComparacoes);
+        
+        
+        // Generate random input array
+        randomArray = new int[numeroComparacoes];
+        Random random = new Random();
+        for (int i = 0; i < numeroComparacoes; i++) {
+            randomArray[i] = random.nextInt();
         }
+        long startTimeRandom = System.nanoTime();
+        cubesort(randomArray, 2);
+        //System.out.println(Arrays.toString(randomArray));
+        long endTimeRandom = System.nanoTime();
+        
+        
+        // Generate decreasing input array
+        int[] decreasingArray = new int[numeroComparacoes];
+        for (int i = 0; i < numeroComparacoes; i++) {
+            decreasingArray[i] = numeroComparacoes - i;
+        } 
+        long startTimeDecres = System.nanoTime();
+        cubesort(decreasingArray, 2);
+        //System.out.println(Arrays.toString(decreasingArray));
+        long endTimeDecres = System.nanoTime();
+        
+        
+        // Best case scenario: array is already sorted
+        int[] bestCaseArray = Arrays.copyOf(randomArray, numeroComparacoes);
+        Arrays.sort(bestCaseArray);
+        long startTimeBest = System.nanoTime();
+        cubesort(bestCaseArray, numeroComparacoes);
+        //System.out.println(Arrays.toString(bestCaseArray));
+        long endTimeBest = System.nanoTime();
+        
+        
+        // Worst case scenario: array is sorted in reverse order
+        int[] worstCaseArray = Arrays.copyOf(randomArray, numeroComparacoes);
+        Arrays.sort(worstCaseArray);
+        for (int i = 0; i < numeroComparacoes / 2; i++) {
+            int temp = worstCaseArray[i];
+            worstCaseArray[i] = worstCaseArray[numeroComparacoes - i - 1];
+            worstCaseArray[numeroComparacoes - i - 1] = temp;
+        }
+        long startTimeWorst = System.nanoTime();
+        cubesort(worstCaseArray, numeroComparacoes);
+        //System.out.println(Arrays.toString(worstCaseArray));
+        long endTimeWorst = System.nanoTime();
+
+        System.out.println("Tempo de execução do random input: " + (endTimeRandom - startTimeRandom) / 1e9 + " segundos");
+        System.out.println("Tempo de execução do decrescente input: " + (endTimeDecres - startTimeDecres) / 1e9 + " segundos");
+        System.out.println("Tempo de execução do melhor caso input: " + (endTimeBest - startTimeBest) / 1e9 + " segundos");
+        System.out.println("Tempo de execução do pior caso input: " + (endTimeWorst - startTimeWorst) / 1e9 + " segundos");
+
+
+
+
     }
 }
