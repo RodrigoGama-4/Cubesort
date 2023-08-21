208e9f4e2713774be30ac0f41d468f3067fe8f4d
rodrigogama <rodrigogamah4@gmail.com>
2023-08-21 10:03:25 -0300

    .

--- Principal.java
+++ Principal.java
@@ -11,6 +11,27 @@ public class Principal {
         
         System.out.println("Tamanho do array: " + numeroComparacoes);
         
+        //Array aleatório
+        randomArray = new int[numeroComparacoes];
+        Random random = new Random();
+        for (int i = 0; i < numeroComparacoes; i++) {
+            randomArray[i] = random.nextInt();
+        }
+        long startTimeRandom = System.nanoTime();
+        ordenar.cubesort(randomArray, 2);
+        //System.out.println(Arrays.toString(randomArray));
+        long endTimeRandom = System.nanoTime();
+
+        //Array decrescente
+        int[] decreasingArray = new int[numeroComparacoes];
+        for (int i = 0; i < numeroComparacoes; i++) {
+            decreasingArray[i] = numeroComparacoes - i;
+        } 
+        long startTimeDecres = System.nanoTime();
+        ordenar.cubesort(decreasingArray, 2);
+
+        //System.out.println(Arrays.toString(decreasingArray));
+        long endTimeDecres = System.nanoTime();
         
        
         // Array ordenado
@@ -21,8 +42,11 @@ public class Principal {
         //System.out.println(Arrays.toString(bestCaseArray));
         long endTimeBest = System.nanoTime();
 
+        
+        System.out.println("Tempo de execução do random input: " + (endTimeRandom - startTimeRandom) / 1e9 + " segundos");
+        System.out.println("Tempo de execução do melhor caso input: " + (endTimeBest - startTimeBest) / 1e9 + " segundos");
+        System.out.println("Tempo de execução do decrescente input: " + (endTimeDecres - startTimeDecres) / 1e9 + " segundos");
 
-        System.out.println("Tempo de execução do random input: " + (endTimeBest - startTimeBest) / 1e9 + " segundos");
        
     }
 }
